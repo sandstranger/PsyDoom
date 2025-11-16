a12b00e8cb2d1f64a9d31625d104c7b6314f824b
sandstranger <sandstranger@gmail.com>
2025-11-16 20:10:23 +0400

    invoke SDL2 main on Android

--- game/Main_StandardCpp.cpp
+++ game/Main_StandardCpp.cpp
@@ -1,9 +1,16 @@
 #include "Doom/psx_main.h"
 #include "PsyDoom/Launcher/Launcher.h"
-
 #include <clocale>
 
+#ifdef ANDROID
+#include <SDL_main.h>
+#endif
+
+#ifdef ANDROID
+int SDL_main(int argc, char **argv) {
+#else
 int main(const int argc, const char* const* const argv) {
+#endif
     // Interpret 'char*' strings as UTF-8 for 'fopen' etc.
     setlocale(LC_ALL, "en_US.UTF-8");
 
