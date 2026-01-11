83e0c43cf2a9181da759bf4d99ef08bc51e84035
sandstranger <sandstranger@gmail.com>
2026-01-12 01:55:38 +0400

    remove extra ifs

--- game/PsyDoom/Input.cpp
+++ game/PsyDoom/Input.cpp
@@ -237,23 +237,17 @@ void rescanGameControllersForced() {
 }
 __attribute__((used)) __attribute__((visibility("default")))
 void onNativePause() {
-#ifndef ANDROID
-    SDL_ShowCursor(SDL_ENABLE);
-#else
     SDL_ShowCursor(SDL_DISABLE);
-#endif
     SDL_SetWindowGrab(Video::gpSdlWindow, SDL_FALSE);
     SDL_SetRelativeMouseMode(SDL_FALSE);
     gbWindowFocusJustLost = true;
 }
 __attribute__((used)) __attribute__((visibility("default")))
 void onNativeResume() {
-#ifdef ANDROID
     SDL_ShowCursor(SDL_DISABLE);
     SDL_SetRelativeMouseMode(SDL_TRUE);
     SDL_SetWindowGrab(Video::gpSdlWindow, SDL_TRUE);
     gbWindowFocusJustLost = false;
-#endif
     // Tell the game to ignore firing until the fire button is released.
     // This prevents clicking on the window with a mouse for example triggering firing.
     gbIgnoreCurrentAttack = true;
